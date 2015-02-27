c54fcc6bcb09e572ad18a17c23ab670ee064113f
vyablokov <v.yablokov@gmail.com>
2015-02-28 00:54:08 +0300

    Some ICs were changed.

--- main.cpp
+++ main.cpp
@@ -2,14 +2,15 @@
 #include "linear_elems.h"
 #include "cubic_elems.h"
 #include <fstream>
+#include <time.h>
 
 int main(/*int argc, char *argv[]*/)
 {
     using namespace std;
 		// FemCubicStatement constructor params:
 		// xbegin,  xend, u'' coef, u' coef, u coef, right side  
-    FemCubicStatement p(1.0, 8.0, 11.0, 3.0, 0.0, 7.0);
-    p.elemsNum = 20; // elemsNum)))
+    FemStatement p(1.0, 8.0, 11.0, 3.0, 0.0, 7.0);
+    p.elemsNum = 20000; // elemsNum)))
 		 // boundary conditions of the first kind - dirichlet
 		 // boundary conditions of the second kind - neumann
     p.leftCond.kind = Condition::Kind::dirichlet; // first kind
@@ -17,7 +18,11 @@ int main(/*int argc, char *argv[]*/)
     p.rightCond.kind  = Condition::Kind::neumann;  // second kind
     p.rightCond.vals.push_back(-6.0); // u'(xend) = u'1 - push_back(u'1)
 
-    auto vec = CubicElems::driver(p);
+    clock_t time;
+    time = clock();
+    auto vec = LinearElems::driver(p);
+    time = clock() - time;
+    std::cout<<"Pure calculation time: "<<(double)time/CLOCKS_PER_SEC<<" s\n";
 
     ofstream file("results.svd");
     for (auto & elem : vec)
